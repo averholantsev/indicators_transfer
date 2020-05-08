ea96ae6755f3dff6b8af2675c7c50419c6202b7c
averholantsev <verkholantsevad@gmail.com>
2020-05-08 18:14:33 +0500

    fix url

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter basename="indicators_transfer">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
